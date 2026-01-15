a3103f3469e2cb25c0522a4b12abc8f9b433f8fc
fumin01@snu.ac.kr <fumin01@snu.ac.kr>
2026-01-15 17:02:57 +0900

    add region api

--- carrot/api.py
+++ carrot/api.py
@@ -4,6 +4,7 @@ from carrot.app.auth.router import auth_router
 from carrot.app.user.router import user_router
 from carrot.app.chat.router import chat_router
 from carrot.app.product.router import product_router
+from carrot.app.region.router import region_router
 # from carrot.app.chat.websocket import chat_ws_router
 
 api_router = APIRouter()
@@ -12,5 +13,6 @@ api_router.include_router(auth_router, prefix="/auth", tags=["auth"])
 api_router.include_router(user_router, prefix="/user", tags=["user"])
 api_router.include_router(chat_router, prefix="/chat", tags=["chat"])
 api_router.include_router(product_router, prefix="/product", tags=["product"])
+api_router.include_router(region_router, prefix="/region", tags=["region"])
 # api_router.include_router(chat_ws_router, prefix="/ws", tags=["chat"])
 
