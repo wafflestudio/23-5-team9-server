c6503c70679b6e40e0e70f735ee84bfdd57f5832
Woohyuk Jang <morrranii@gmail.com>
2026-01-16 20:46:34 +0900

    Fix broken migration

--- carrot/db/migrations/versions/a039904b38fe_.py
+++ carrot/db/migrations/versions/a039904b38fe_.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'a039904b38fe'
-down_revision: Union[str, Sequence[str], None] = '4ade29ccdc03'
+down_revision: Union[str, Sequence[str], None] = '1b157524498d'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
