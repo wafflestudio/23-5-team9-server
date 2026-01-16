3dab2a023ffd24d8c2a0db3ea87e59c55a174ced
Xeavveed <ericsong2004@snu.ac.kr>
2026-01-16 14:40:58 +0900

    api image import 주석처리

--- carrot/api.py
+++ carrot/api.py
@@ -5,7 +5,7 @@ from carrot.app.user.router import user_router
 from carrot.app.chat.router import chat_router
 from carrot.app.product.router import product_router
 from carrot.app.region.router import region_router
-from carrot.app.image.router import image_router
+# from carrot.app.image.router import image_router
 # from carrot.app.chat.websocket import chat_ws_router
 
 api_router = APIRouter()
@@ -14,6 +14,6 @@ api_router.include_router(auth_router, prefix="/auth", tags=["auth"])
 api_router.include_router(user_router, prefix="/user", tags=["user"])
 api_router.include_router(chat_router, prefix="/chat", tags=["chat"])
 api_router.include_router(product_router, prefix="/product", tags=["product"])
-api_router.include_router(image_router, prefix="/image", tags=["image"])
+# api_router.include_router(image_router, prefix="/image", tags=["image"])
 api_router.include_router(region_router, prefix="/region", tags=["region"])
 # api_router.include_router(chat_ws_router, prefix="/ws", tags=["chat"])
