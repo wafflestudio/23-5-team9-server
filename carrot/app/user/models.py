d3122a6e6cbc051aa2054a1c4c1445f8b5627238
Woohyuk Jang <morrranii@gmail.com>
2026-01-16 20:20:27 +0900

    Fix missing paranthesis

--- carrot/app/user/models.py
+++ carrot/app/user/models.py
@@ -42,9 +42,12 @@ class User(Base):
         "SocialAccount", back_populates="user"
     )
 
-    sent_ledgers: Mapped["Ledger"] = relationship("Ledger", foreign_keys="[Ledger.user_id]", back_populates="user")
+    sent_ledgers: Mapped["Ledger"] = relationship(
+        "Ledger", foreign_keys="[Ledger.user_id]", back_populates="user"
+    )
     received_ledgers: Mapped["Ledger"] = relationship(
         "Ledger", foreign_keys="[Ledger.receive_user_id]", back_populates="receive_user"
+    )
     # Set relationships to 1:1 ChatRoom
     chat_rooms_v1: Mapped[list["ChatRoom"]] = relationship(
         "ChatRoom", foreign_keys="[ChatRoom.user_one_id]", back_populates="user_one"
