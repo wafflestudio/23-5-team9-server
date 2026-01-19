b31e73aaba7fe02442161d4bb9a7a401639b776a
Woohyuk Jang <morrranii@gmail.com>
2026-01-20 00:16:53 +0900

    Add region data to model - automatically add data via alembic

--- carrot/app/region/models.py
+++ carrot/app/region/models.py
@@ -1,10 +1,25 @@
 import uuid
-from sqlalchemy import String, Integer, ForeignKey
+from sqlalchemy import String, Integer, ForeignKey, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from carrot.db.common import Base
+
 
 class Region(Base):
     __tablename__ = "region"
 
-    id: Mapped[str] = mapped_column(String(36), primary_key=True, index=True, default=lambda: str(uuid.uuid4()))
-    name: Mapped[str] = mapped_column(String(20), unique=True, index=True)
+    id: Mapped[str] = mapped_column(
+        String(36), primary_key=True, index=True, default=lambda: str(uuid.uuid4())
+    )
+    sido: Mapped[str] = mapped_column(String(20), nullable=False, index=True)
+    sigugun: Mapped[str] = mapped_column(String(20), nullable=False, index=True)
+    dong: Mapped[str] = mapped_column(String(20), nullable=False, default="")
+
+    __table_args__ = (
+        UniqueConstraint(
+            "sido", "sigugun", "dong", name="uix_region_sido_sigugun_dong"
+        ),
+    )
+
+    @property
+    def name(self):
+        return f"{self.sido} {self.sigugun} {self.dong}"
