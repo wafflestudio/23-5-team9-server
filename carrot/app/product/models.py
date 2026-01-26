e970dc769e5e99aeea0aac7692995668868ed1a5
fumin01@snu.ac.kr <fumin01@snu.ac.kr>
2026-01-26 20:42:46 +0900

    build auction models

--- carrot/app/product/models.py
+++ carrot/app/product/models.py
@@ -9,24 +9,31 @@ from carrot.app.user.models import User
 from carrot.app.image.models import ProductImage
 from carrot.app.category.models import Category
 from carrot.app.region.models import Region
+from carrot.app.auction.models import Auction, AuctionStatus
     
 class Product(Base):
     __tablename__ = "product"
 
     id: Mapped[str] = mapped_column(String(36), primary_key=True, index=True, default=lambda: str(uuid.uuid4()))
     owner_id:  Mapped[str] = mapped_column(String(36), ForeignKey("user.id", ondelete="CASCADE"), nullable=False, index=True)
+    category_id: Mapped[str] = mapped_column(String(36), ForeignKey("category.id", ondelete="CASCADE"), nullable=False, index=True)
+    region_id: Mapped[str] = mapped_column(String(36), ForeignKey("region.id", ondelete="CASCADE"), nullable=False, index=True)
+
     title: Mapped[str] = mapped_column(String(50))
     image_ids: Mapped[list] = mapped_column(JSON, default=[], nullable=False)
     content: Mapped[str | None] = mapped_column(String(500))
     price: Mapped[int] = mapped_column(Integer, nullable=False)
     like_count: Mapped[int] = mapped_column(Integer, default=0)
-    category_id: Mapped[str] = mapped_column(String(36), ForeignKey("category.id", ondelete="CASCADE"), nullable=False, index=True)
-    region_id: Mapped[str] = mapped_column(String(36), ForeignKey("region.id", ondelete="CASCADE"), nullable=False, index=True)
     is_sold: Mapped[bool] = mapped_column(Boolean, default=False)
 
-    user: Mapped[Category] = relationship("User")
+    user: Mapped[User] = relationship("User")
     category: Mapped[Category] = relationship("Category")
     region: Mapped[Region] = relationship("Region")
+    
+    auction: Mapped["Auction"] = relationship("Auction", back_populates="product")
+    @property
+    def current_auction(self):
+        return next((a for a in self.auctions if a.status == AuctionStatus.ACTIVE), None)
     
 class UserProduct(Base):
     __tablename__ = "user_product"
